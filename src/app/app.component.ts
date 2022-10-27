b9f639ebb6ac4188b90e8c3437b2058a9dd9b56b
Rajasaikiran Vemula <rvemula@ariqt.com>
2022-10-27 12:21:46 +0530

    Rating App

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'Rating';
+
+  public star = [1, 2, 3, 4, 5];
+  public selected = 0;
+
+  public updateRating(rate: any) {
+    this.selected = rate;
+  }
 }
